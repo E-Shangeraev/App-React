792728bcd71ba394155e0694a0731be001e870ab
Eldar Shangeraev <e.shangeraev@gmail.com>
2020-07-14 16:28:08 +0700

    "master"

---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+    <div className="profile">
+        <div>
+          <img src="https://www.w3schools.com/howto/img_snow_wide.jpg"></img>
+        </div>
+        <div>
+          ava + decription
+        </div>
+        <div>
+          My posts
+          <div>
+            New post
+          </div>
+          <ul>
+            <li>post 1</li>
+            <li>post 2</li>
+          </ul>
+        </div>
+    </div>
+    )
+}
+
+export default Profile
